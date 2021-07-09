b312e9edab14a2aebeb611fdd847b4bbeca4bab1
MKH-Beldi <65089678+MKH-Beldi@users.noreply.github.com>
2021-07-09 13:41:50 +0100

    jwt Token

---
+++ src/Controller/Security/SecurityController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Controller\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/api/login", name="api_login", methods={"POST"})
+     */
+    public function login(Request $request)
+    {
+        $user = $this->getUser();
+
+        return $this->json([
+            'username' => $user->getUsername(),
+            'roles' => $user->getRoles(),
+        ]);
+    }
+}
